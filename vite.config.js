4cfc3b40588d998a8edb86241f249df888852e79
Amit Ayre <amit.ayre@dialpad.com>
2025-11-05 03:04:44 +0530

    Update base path for GitHub Pages deployment to v2 repo

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig(({ command }) => ({
   plugins: [vue()],
-  base: command === 'build' ? '/daart-onboarding-prototype/' : '/',
+  base: command === 'build' ? '/daart-onboarding-prototype-v2/' : '/',
   server: {
     port: 3000
   }
